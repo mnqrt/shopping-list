effce7cf2412b2a47b7da4b2b8ed2ab0ccacd222
Adrian <adrian.hamzah15@gmail.com>
2023-11-15 14:43:25 +0700

    udah kelar

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product, get_product_json, add_product_ajax 
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product, get_product_json, add_product_ajax, create_product_flutter 
 
 app_name = 'main'
 
@@ -17,4 +17,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
